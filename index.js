2b69a76a2f9af99f6f0689dea49953c92f8394ba
varuscritical900 <varuscrtical900@gmail.com>
2023-02-17 19:01:45 +0800

    update jsonserver files

---
+++ index.js
@@ -0,0 +1,11 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./database/db.json');
+const middleware = jsonServer.defaults();
+const port = process.env.PORT || 5000;
+
+server.use(middleware);
+server.use(router);
+server.listen(port, ()=>{
+    console.log('Json Server is now running')
+});
